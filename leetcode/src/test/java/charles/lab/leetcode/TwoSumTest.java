9c9a829956a34477d9d7bf86153fabb20f6d4e37
Charles <sabaao35@gmail.com>
2019-01-14 19:29:12 +0800

    TwoSum practice

---
+++ leetcode/src/test/java/charles/lab/leetcode/TwoSumTest.java
@@ -0,0 +1,30 @@
+package charles.lab.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class TwoSumTest {
+	
+	TwoSum ts = new TwoSum();
+	
+	@Test
+	public void solution_OnlyTwoElement() {
+		int[] nums = {0,1};
+		
+		assertTrue(Arrays.equals(nums, ts.solution(nums, 1))); 
+	}
+	
+	@Test
+	public void solution_ThreeElement_FirstTwo() {
+		int[] nums = {0,1,2};
+		int[] except = {0,1};
+		assertTrue(Arrays.equals(except, ts.solution(nums, 1)));
+	 
+	}
+	
+
+}
