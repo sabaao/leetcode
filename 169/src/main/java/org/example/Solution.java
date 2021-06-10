d701a0b3ff577e037f3ca6e96c0ee1883aa632ee
Charles <charles@cathayholdings.com.tw>
2021-06-10 22:06:46 +0800

    use array sort

--- 169/src/main/java/org/example/Solution.java
+++ 169/src/main/java/org/example/Solution.java
@@ -7,24 +7,9 @@ import java.util.Map;
 
 public class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer,Integer> result = new HashMap<Integer, Integer>();
-        for(int num: nums){
-            if(result.get(num)!=null){
-                result.put(num,result.get(num)+1);
-            }else{
-                result.put(num,1);
-            }
-        };
-        int acutal = 0;
-        int times = 0;
-
-        for(Map.Entry<Integer,Integer> entry: result.entrySet()){
-            if(entry.getValue()>times){
-                acutal = entry.getKey();
-                times = entry.getValue();
-            }
-        }
-
-        return acutal;
+        Arrays.sort(nums);
+        int index = nums.length/2;
+        int result = nums[index];
+        return result;
     }
 }
